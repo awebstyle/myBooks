4624f22872f82012f4a0f3ae17a85178294b95d3
awebstyle <gisele@awebstyle.be>
2022-09-26 10:54:40 +0200

    création d'un composant BookResource
    séparation de la logique en créant un composant spécifique pour les livres

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,17 +1,24 @@
 <template>
     <h1>Dernières lectures</h1>
     <ul>
-        <li v-for="storedBook in storedBooks" :key="storedBook.id">
-            <h2>{{ storedBook.title }}</h2>
-            <h3>{{ storedBook.author }} - {{ storedBook.edition }}</h3>
-            <p>Résumé : {{ storedBook.summary }}</p>
-            <p>Avis : {{ storedBook.opinion }}</p>
-        </li>
+      <book-resource v-for="book in storedBooks" :key="book.id"
+        :title="book.title"
+        :author="book.author"
+        :edition="book.edition"
+        :summary="book.summary"
+        :opinion="book.opinion"
+        :link="book.link"
+      >
+      </book-resource>  
     </ul>
 </template>
 
 <script>
+    import BookResource from './components/books/BookResource.vue';
     export default {
+        components: {
+            BookResource
+        },
         data(){
             return {
                 storedBooks: [
@@ -21,7 +28,8 @@
                     author: 'Stendhal',
                     edition: 'Les Cent Chefs-d\'oeuvre',
                     summary: 'Julien Sorel, fils de charpentier, doté de capacités intellectuelles supérieurs, tente de s\'élever dans la société. Passionné par Napoléon, il rêve d\'une grande destinée en revêtant l\'habit de soldat (rouge) mais c\'est celui du clergé (noir) qu\'il sera amené à revêtir.',
-                    opinion: 'Très beaux textes.  Début très prenant mais l\'histoire devient moins captivante au fil de la lecture.  Ce roman reste toutefois un grand classique très intéressant et, sans conteste, à lire.'
+                    opinion: 'Très beaux textes.  Début très prenant mais l\'histoire devient moins captivante au fil de la lecture.  Ce roman reste toutefois un grand classique très intéressant et, sans conteste, à lire.',
+                    link: 'https://awebstyle.be'
                 },
                 {
                     id: 'clMalrauxCH',
@@ -29,7 +37,8 @@
                     author: 'Malraux',
                     edition: 'folio',
                     summary: 'Pas encore de résumé ...',
-                    opinion: 'Pas encore d\'opinion.' 
+                    opinion: 'Pas encore d\'opinion.',
+                    link: 'https://awebstyle.be' 
                 }
                 ]
             }
---
+++ prj-cmp-01-starting-setup/src/components/books/BookResource.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3>{{ title }} - {{ author }} - {{ edition }}</h3>
+                <button>Supprimer</button>
+            </header>
+        </div>
+        <p>{{ summary }}</p>
+        <p>{{ opinion }}</p>
+        <a :href="link" target="blank">Voir le résumé en images</a>
+    </li>
+</template>
+
+<script>
+    export default {
+        props: ['title', 'auhtor', 'edition', 'summary', 'opinion', 'link']
+    }
+</script>
